5a489f71531b27edcac0c483e88966bfe4d06690
KrzysiekLasko <KrzysiekLasko25@gmail.com>
2023-03-04 19:14:33 +0100

    Dzien 4 Zadanie Domowe

---
+++ HomeWorkDay4/HomeWorkDay4/Program.cs
@@ -0,0 +1,26 @@
+﻿/// Zadanie domowe dzien 4
+
+var herAge = 33;
+var herName = "Ewa";
+var herSex = "Kobieta";
+if (herAge == 33)
+{
+    if (herName == "Ewa")
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+}
+if (herSex == "Kobieta")
+{
+    if (herAge > 30)
+    {
+        Console.WriteLine("Kobieta powyzej 30 lat");
+    }
+}
+if (herAge > 16)
+{
+    if (herSex == "Kobieta")
+    {
+        Console.WriteLine("Pelnoletnia kobieta");
+    }
+}
