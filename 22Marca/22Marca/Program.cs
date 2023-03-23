3a54df19f50058795e3bc75b6db31a5d5d985632
KrzysiekLasko <krzysieklasko25@gmail.com>
2023-03-23 12:55:09 +0100

    Zadanie domowe Dzien 6

---
+++ 22Marca/22Marca/Program.cs
@@ -0,0 +1,20 @@
+﻿List<string> daysOfWeek = new List<string>();
+daysOfWeek.Add("poniedzialek");
+daysOfWeek.Add("wtorek");
+daysOfWeek.Add("sroda");
+daysOfWeek.Add("czwartek");
+daysOfWeek.Add("piatek");
+daysOfWeek.Add("sobota");
+daysOfWeek.Add("niedziela");
+
+Console.WriteLine(daysOfWeek[2]);
+Console.WriteLine(daysOfWeek[3]);
+Console.WriteLine("-----------");
+
+
+for (int i = 0; i < daysOfWeek.Count; i++)
+{
+    Console.WriteLine(daysOfWeek[i]);
+}
+
+   
